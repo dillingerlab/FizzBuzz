56f08035c321856c283379d404acd65cd9c413db
Austin <shuffletick@gmail.com>
2023-08-07 21:32:30 -0600

    init; with comments

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def main():
+    """Description of the function"""
+
+
+if __name__ == "__main__":
+    # loop
+    # itertools
+    # namedTuple?
+    # defaultDict? just for fun
+    main()
+
