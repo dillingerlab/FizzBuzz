45b5d36c0ba8e52e8503341a447c11711aaf84ca
Austin <shuffletick@gmail.com>
2023-08-22 20:26:20 -0600

    solve with basic loop

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-def main():
-    """Description of the function"""
+def fizzbuzz_loop(end_value: int):
+    end_value += 1
+    for num in range(1, end_value):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        elif num % 5 == 0:
+            print("buzz")
+        elif num % 3 == 0:
+            print("fizz") 
+        else:
+            print(num)
 
 
 if __name__ == "__main__":
-    # loop
     # itertools
     # namedTuple?
     # defaultDict? just for fun
-    main()
+    fizzbuzz_loop(33)
 
